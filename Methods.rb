55e21a4792cd48a2ec9b10462ef9f5763272fbc5
thiagomsantanna <thiagomsantanna11@gmail.com>
2021-10-02 23:56:09 -0300

    many arguments method

--- Methods.rb
+++ Methods.rb
@@ -6,3 +6,22 @@ teste = {"negocio" => 3, "treco" => 4}
 DefaultValueParameter() # prints SalveSalve
 DefaultValueParameter(teste) # prints {"negocio"=>3, "treco"=>4}
 DefaultValueParameter("DaleDale") # prints DaleDale
+
+
+# *friends is a parameter that could receive how many arguments u pass for him, he does that transforming all arguments in one array, independent if u passed 1 arguments or
+def TestManyArguments(*friends) 
+    puts "oia só - #{friends}"
+end
+
+TestManyArguments("Thiagao", "Pedrao", "Marcela meu amor") # will print "oia só ["Thiagao", "Pedrao", "Marcela meu amor"]"
+TestManyArguments("thaigo") # will print "oia só ["thaigo"]"
+
+
+#
+
+
+def ManyArguments(*friends)
+    friends.each do |friend| puts "Os d verdade -> #{friend}" end
+end
+
+ManyArguments("Thiagao", "Marcela")
